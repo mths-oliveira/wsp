2ce49accb92432d4d627f3e805504d789c724dbf
Matheus Oliveira <contato.mths@outlook.com>
2022-08-28 11:18:17 -0300

    add title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,6 +12,7 @@ export default class Document extends NextDocument {
             href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap"
             rel="stylesheet"
           />
+          <title>Conversor de Moedas - Matheus Oliveira</title>
         </Head>
         <body>
           <ColorModeScript initialColorMode="dark" />
